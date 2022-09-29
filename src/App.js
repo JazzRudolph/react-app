79e11158473f851c8a7ca8ee9bb4c2c026919194
Jazz Rudolph <jazzrudolph92@outlook.com>
2022-09-29 00:10:14 -0400

    Setting up first function.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jazz
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'><h1>Todays Agenda</h1>
+    I am a div!</div>
+  )
 }
 
-export default App;
+export default App
